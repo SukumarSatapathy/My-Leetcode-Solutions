897dca558e243d1b16340abe585e4384f6a80d60
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-08 23:10:41 +0530

    Update Two Sum.py

--- Arrays & Hashing/Two Sum/Two Sum.py
+++ Arrays & Hashing/Two Sum/Two Sum.py
@@ -1,3 +1,8 @@
+'''
+Runtime: 117 ms, faster than 49.53% of Python3 online submissions for Two Sum.
+Memory Usage: 15.3 MB, less than 23.96% of Python3 online submissions for Two Sum.
+'''
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = {}
