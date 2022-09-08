2a7adaef0c497482912d9a0d08ee3d8a801b7c50
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-09 00:48:40 +0530

    Create  Set Matrix Zeroes.py

---
+++ Arrays & Hashing/Set Matrix Zeroes/Set Matrix Zeroes.py
@@ -0,0 +1,22 @@
+'''
+Runtime: 135 ms, faster than 92.23% of Python3 online submissions for Set Matrix Zeroes.
+Memory Usage: 14.8 MB, less than 54.57% of Python3 online submissions for Set Matrix Zeroes.
+'''
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        row = [1]*len(matrix)
+        col = [1]*len(matrix[0])
+        for i in range(len(row)):
+            for j in range(len(col)):
+                if matrix[i][j] == 0:
+                    row[i] = 0
+                    col[j] = 0
+        for i in range(len(row)):
+            for j in range(len(col)):
+                if row[i] == 0 or col[j] == 0:
+                    matrix[i][j] = 0
