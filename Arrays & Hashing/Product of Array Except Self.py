47a0f7d9fd941b60983578bed352466d6c638457
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-10 18:43:40 +0530

    Create Product of Array Except Self.py

---
+++ Arrays & Hashing/Product of Array Except Self.py
@@ -0,0 +1,19 @@
+'''
+Time : O(n)
+Space : O(n)
+'''
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prefix_product = 1
+        postfix_product = 1
+        result = [0]*n
+        for i in range(n):
+            result[i] = prefix_product
+            prefix_product *= nums[i]
+        for i in range(n-1,-1,-1):
+            result[i] *= postfix_product
+            postfix_product *= nums[i]
+        return result
+        
