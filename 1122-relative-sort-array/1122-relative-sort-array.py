9365c9e5fcc5a97b44f93b950df41e7f38db6cd5
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-16 12:26:04 +0530

    Time: 85 ms (17.42%), Space: 13.9 MB (77.18%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        freq = {}
-        result, rem = [], [0]*max(arr1)
+        freq = {} #to store the frequency of each element of arr2 in arr1
+        result, rem = [], [0]*max(arr1) #rem stores the freq of the elememts present in arr1 alone
         for num in arr2:
             freq[num] = 0
         
@@ -12,7 +12,7 @@ class Solution:
                 rem[num-1] += 1 
         
         for num in arr2:
-            f = freq.pop(num)
+            f = freq.pop(num) #removes and returns the value of freq[num]
             result.extend([num]*f)
         
         for i,f in enumerate(rem):
