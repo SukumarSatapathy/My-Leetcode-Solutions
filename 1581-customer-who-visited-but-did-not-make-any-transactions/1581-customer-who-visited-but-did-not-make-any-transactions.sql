0d57a8f65b495a9f5b309319d09eb8ef7e0812fa
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-09-18 00:54:01 +0530

    Time: 1549 ms (43.88%), Space: 0B (100.00%) - LeetHub

---
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,10 @@
+SELECT
+	customer_id,
+	COUNT(Visits.visit_id) AS count_no_trans
+FROM
+	visits
+LEFT JOIN Transactions
+	ON Visits.visit_id = Transactions.visit_id
+WHERE 
+	Transactions.visit_id IS NULL
+GROUP BY customer_id
