ba00514b0406c217fafc898980ee0d7098cd6e46
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-10-23 23:03:04 +0530

    Time: 507 ms (32.14%), Space: 15.1 MB (99.41%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -2,8 +2,6 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         nums.sort()
         n = len(nums)
-        flag = False
-        pos = 1
         for i in range(n):
             if i-1 >= 0 and nums[i] == nums[i-1]:
                 duplicate = nums[i]
