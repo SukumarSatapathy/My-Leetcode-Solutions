eebf37a9931a75a2627beaa57bd3fabfd401fe01
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2022-10-28 22:36:43 +0530

    Time: 257 ms (26.66%), Space: 19.6 MB (29.31%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = {}
+        for s in strs:
+            count = [0]*26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            if tuple(count) not in ans:
+                ans[tuple(count)] = [s]
+            else:
+                ans[tuple(count)].append(s)
+        return ans.values()
