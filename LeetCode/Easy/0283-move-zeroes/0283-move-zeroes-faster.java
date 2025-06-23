aa46399f15ccf7eb72d95ecdce6acf63d8e041ac
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2025-06-24 01:33:30 +0530

    Time: 1 ms (99.74%), Space: 46.3 MB (25.83%) - LeetHub

---
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes-faster.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
