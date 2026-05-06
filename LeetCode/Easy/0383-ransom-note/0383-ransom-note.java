80952851e91c32dc9a2b501b717dec51ba799ab3
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-05-06 19:29:11 +0530

    Easy Hash Table .java 1 ms (99.9%) 46.4 MB (69.59%)

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.java
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.java
@@ -2,15 +2,17 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         // Solution 3 - most optimized
         // two loops, 1 array to store count
+        if (ransomNote.length() > magazine.length()) return false;
+
         int[] freq = new int[26];
         for (char c : magazine.toCharArray()) {
             freq[c - 'a']++;
         }
         for (char c : ransomNote.toCharArray()) {
-            freq[c - 'a']--;
-            if (freq[c - 'a'] < 0) {
+            if (freq[c - 'a'] == 0) {
                 return false;
             }
+            freq[c - 'a']--;
         }
         return true;
     }
