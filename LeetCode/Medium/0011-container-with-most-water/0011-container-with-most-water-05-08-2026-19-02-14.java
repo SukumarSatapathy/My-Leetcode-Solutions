7d0bd157c75ec7e03d07a19e2bcdd5a94c7e171d
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-05-08 19:02:14 +0530

    Medium Array .java 5 ms (82.9%) 77.4 MB (47.99%)

---
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water-05-08-2026-19-02-14.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int most = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            most = Math.max(most, area); // greedy choice
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+        return most;
+    }
+}
