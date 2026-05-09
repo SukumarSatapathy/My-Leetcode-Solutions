f1944ec989c170872816708686aa87685a7a9944
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-05-09 16:54:41 +0530

    Easy Array .java 3 ms (18.38%) 47.9 MB (37.42%)

---
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes-05-09-2026-16-54-41.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        // reader-writer
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != 0) {
+                int temp = nums[slow];
+                nums[slow] = nums[fast];
+                nums[fast] = temp;
+                slow++;
+            }
+        }
+    }
+}
