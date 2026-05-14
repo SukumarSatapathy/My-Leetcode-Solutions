f6bd31b6a39a0a2eb4d601a5ff7605a9e4658f98
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-05-14 14:59:38 +0530

    Easy Array .java 0 ms (100%) 46.7 MB (94.77%)

---
+++ LeetCode/Easy/2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer-05-14-2026-14-59-37.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        // find the 1st occur and last occur of 0
+        // calculate count of +ve or -ve after that
+        int positiveCount = nums.length - searchFirstGreater(nums, 0);
+        return Math.max(searchFirstOrInsert(nums, 0), positiveCount);
+    }
+
+    public int searchFirstOrInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid, pos = nums.length; // O(log n) time
+        // first i where arr[i] >= target
+        while (l <= r) {
+            mid = l + (r - l)/2;
+            if (nums[mid] >= target) {
+                pos = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return pos;
+    }
+
+    public int searchFirstGreater(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, mid, pos = nums.length; // O(log n) time
+        // first i where arr[i] > target
+        while (l <= r) {
+            mid = l + (r - l)/2;
+            if (nums[mid] > target) {
+                pos = mid;
+                r = mid - 1;
+            }
+            else {
+                l = mid + 1;
+            }
+        }
+        return pos;
+    }
+}
