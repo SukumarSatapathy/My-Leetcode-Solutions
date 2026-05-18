019635b3d8fa367a46b6a90c71453996d2a94645
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-05-18 16:10:23 +0530

    Medium String .java 13 ms (76.53%) 46.3 MB (79.24%)

---
+++ LeetCode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length-05-18-2026-16-10-22.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxVowels(String s, int k) {
+        int windowCount = 0, n = s.length();
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) windowCount++;
+        }
+        int maxCount = windowCount;
+        for (int i = k; i < n; i++) {
+            if (isVowel(s.charAt(i))) windowCount++;
+            if (isVowel(s.charAt(i - k))) windowCount--;
+            maxCount = Math.max(maxCount, windowCount);
+            if (maxCount == k) return k; // early return
+        }
+        return maxCount;
+    }
+
+    public boolean isVowel(char ch) {
+    return ch == 'a' ||
+           ch == 'e' ||
+           ch == 'i' ||
+           ch == 'o' ||
+           ch == 'u';
+    }
+}
