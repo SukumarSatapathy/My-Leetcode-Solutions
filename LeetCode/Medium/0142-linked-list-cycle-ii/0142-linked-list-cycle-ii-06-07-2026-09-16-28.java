30621d6c6715b513aecfcce404efc4f8d459e21e
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-06-07 09:16:28 +0530

    Medium Hash Table .java 0 ms (100%) 46.5 MB (68.92%)

---
+++ LeetCode/Medium/0142-linked-list-cycle-ii/0142-linked-list-cycle-ii-06-07-2026-09-16-28.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode curr = head;
+                while (curr != slow) {
+                    slow = slow.next;
+                    curr = curr.next;
+                }
+                return curr;
+            }
+        }
+        return null;
+    }
+}
