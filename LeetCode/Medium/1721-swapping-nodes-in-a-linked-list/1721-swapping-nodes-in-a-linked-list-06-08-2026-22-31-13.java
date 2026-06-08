5d1c4cc5bfbfac623d70c55a4e27c299dcb442f9
Sukumar Satapathy <111305599+SukumarSatapathy@users.noreply.github.com>
2026-06-08 22:31:14 +0530

    Medium Linked List .java 1 ms (100%) 120.2 MB (6.95%)

---
+++ LeetCode/Medium/1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list-06-08-2026-22-31-13.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode first = head, second = head, curr = head;
+        for (int i = 1; i < k; i++) {
+            first = first.next;
+            curr = curr.next;
+        }
+
+        while (curr.next != null) {
+            curr = curr.next;
+            second = second.next;
+        }
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        return head;
+    }
+}
